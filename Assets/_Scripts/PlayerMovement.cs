99bfba84f941f38feacd4e22745e75e6fd165aad
Rumsse <mwieczorek5@edu.cdv.pl>
2025-04-01 21:55:44 +0200

    project added

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float turnSpeed = 360;
+    private Vector3 input;
+
+    void Update()
+    {
+        GatherInput();
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GatherInput()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    void Look()
+    {
+        if (input != Vector3.zero)
+        {
+            var relative = (transform.position + input.ToIso()) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
+    }
+
+}
