b14f3958fb00f5959764584a6222f389857262bb
Rumsse <mwieczorek5@edu.cdv.pl>
2025-04-08 15:55:04 +0200

    testing diffrent spawner
    zapis skryptu w 2 wersjach

--- Assets/_Scripts/Enemies/EnemySpawner.cs
+++ Assets/_Scripts/Enemies/EnemySpawner.cs
@@ -1,9 +1,37 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota;
+        public float spawnInterval;
+        public int spawnCount;
+    }
+
+    [System.Serializable]
+    public class  EnemyGroup
+    {
+        public string enemyName;
+        public int enemyCount;
+        public int spawnCount;
+        public GameObject enemyPrefab;
+    }
+
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    [Header("Spawner Attributes")]
+    float spawnTimer;
+    public float waveInterval;
+
+    [Header("----Prototype 1----")]
     public GameObject enemyPrefab;
     private Queue<GameObject> enemyPool = new Queue<GameObject>();
 
@@ -12,6 +40,8 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] private int poolSize = 20;
     [SerializeField] private float spawnRadius = 1.5f; //dystans od gracza
+
+
 
     private void Awake()
     {
@@ -74,6 +104,75 @@ public class EnemySpawner : MonoBehaviour
         }
 
         return player.position;
+    } 
+
+
+
+
+
+    void Startt()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        CalculateWaveQuota();
+    }
+
+    void Update()
+    {
+        if(currentWaveCount < waves.Count && waves[currentWaveCount].spawnCount == 0)
+        {
+            StartCoroutine(BeginNextWave());
+        }
+
+        spawnTimer = Time.deltaTime;
+
+        if(spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            spawnTimer = 0f;
+            SpawnEnemies();
+        }
+    }
+
+    IEnumerator BeginNextWave()
+    {
+        yield return new WaitForSeconds(waveInterval);
+
+        if(currentWaveCount < waves.Count - 1)
+        {
+            currentWaveCount++;
+            CalculateWaveQuota();
+        }
+    }
+
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuoata = 0;
+        foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveQuoata += enemyGroup.enemyCount;
+        }
+
+        waves[currentWaveCount].waveQuota = currentWaveQuoata;
+        Debug.LogWarning(currentWaveQuoata);
+    }
+
+
+    void SpawnEnemies()
+    {
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota)
+        {
+            foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                if(enemyGroup.spawnCount < enemyGroup.enemyCount)
+                {
+                    Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+                    Instantiate(enemyGroup.enemyPrefab, spawnPosition, Quaternion.identity);
+
+                    enemyGroup.spawnCount++;
+                    waves[currentWaveCount].spawnCount++;
+                }
+            }
+        }
     }
 
 }
